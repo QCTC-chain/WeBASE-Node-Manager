db06c6f04acae61960472443582860b1db9d9806
yyzhangyang <zy871029@126.com>
2023-09-13 16:35:30 +0800

    feat: 引入sa-token、security能力，先注掉swagger2文档的相关引用，解决运行失败的问题

--- src/main/java/com/webank/webase/node/mgr/appintegration/AppIntegrationController.java
+++ src/main/java/com/webank/webase/node/mgr/appintegration/AppIntegrationController.java
@@ -23,24 +23,16 @@ import com.webank.webase.node.mgr.base.entity.BasePageResponse;
 import com.webank.webase.node.mgr.base.entity.BaseResponse;
 import com.webank.webase.node.mgr.base.enums.AppType;
 import com.webank.webase.node.mgr.base.exception.NodeMgrException;
-import com.webank.webase.node.mgr.config.properties.ConstantProperties;
 import com.webank.webase.node.mgr.tools.JsonTools;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
-import javax.validation.Valid;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * application integration controller.
