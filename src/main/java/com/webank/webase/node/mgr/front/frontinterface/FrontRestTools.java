0fa8d06a5595ab3b12992eb0cc6e8f305a76f953
yyzhangyang <zy871029@126.com>
2023-09-25 11:20:27 +0800

    feat : 节点管理服务调用前置时，添加sa-token的key和value

--- src/main/java/com/webank/webase/node/mgr/front/frontinterface/FrontRestTools.java
+++ src/main/java/com/webank/webase/node/mgr/front/frontinterface/FrontRestTools.java
@@ -14,6 +14,7 @@
 
 package com.webank.webase.node.mgr.front.frontinterface;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.webank.webase.node.mgr.base.code.ConstantCode;
 import com.webank.webase.node.mgr.base.enums.DataStatus;
@@ -184,6 +185,10 @@ public class FrontRestTools {
 
     private static Map<String, FailInfo> failRequestMap = new HashMap<>();
 
+    // sa-token
+    private static String tokenKey;
+    private static String tokenValue;
+
 
     /**
      * append groupId to uri.
@@ -314,10 +319,30 @@ public class FrontRestTools {
     public static HttpEntity buildHttpEntity(Object param) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        try {
+            // sa-token的token鉴权字段，用于front调用sign
+            String tokenKey = StpUtil.getTokenName();
+            String tokenValue = StpUtil.getTokenValueNotCut();
+
+            if (!StringUtils.isEmpty(tokenValue)) {
+                FrontRestTools.tokenKey = tokenKey;
+                FrontRestTools.tokenValue = tokenValue;
+            }
+        } catch (Exception e) {
+            log.info("get token key or value err, ignore, err msg:{}", e.getMessage());
+        }
+
+        if (!StringUtils.isEmpty(FrontRestTools.tokenValue)) {
+            headers.add("tokenKey", FrontRestTools.tokenKey);
+            headers.add("tokenValue", FrontRestTools.tokenValue);
+        }
+
         String paramStr = null;
         if (Objects.nonNull(param)) {
             paramStr = JsonTools.toJSONString(param);
         }
+        log.info("buildHttpEntity for front, headers:{}", headers);
         HttpEntity requestEntity = new HttpEntity(paramStr, headers);
         return requestEntity;
     }
